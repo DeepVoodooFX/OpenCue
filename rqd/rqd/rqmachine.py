0e26bbca1dd493f466f99ac92799a28792f08bee
jason <jasonmads@gmail.com>
2024-01-06 10:26:59 -0800

    nvlink booking if requesting 2 gpus.

--- rqd/rqd/rqmachine.py
+++ rqd/rqd/rqmachine.py
@@ -913,6 +913,17 @@ class Machine(object):
             log.critical(err)
             raise rqd.rqexceptions.CoreReservationFailureException(err)
 
+        nvlink_pairs = subprocess.getoutput('/ParkCounty/apps/lnx/nube-tools/linkQuery').splitlines()
+        nvlink_pairs = [(int(pair.split(',')[0]), int(pair.split(',')[1])) for pair in nvlink_pairs]
+        nvlink_pairs = list(set([tuple(sorted(pair)) for pair in nvlink_pairs]))
+
+        if 2 == reservedGpus:
+            for pair in nvlink_pairs:
+                if pair[0] in self.__gpusets and pair[1] in self.__gpusets:
+                    self.__gpusets.remove(pair[0])
+                    self.__gpusets.remove(pair[1])
+                    return ','.join([str(pair[0]), str(pair[1])])
+
         gpusets = []
         for _ in range(reservedGpus):
             gpu = self.__gpusets.pop()
